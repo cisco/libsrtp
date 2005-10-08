a062be14ae2758d742663019d237242418030f60
Marcus Sundberg <adamel@example.net>
2005-10-08 16:58:13 +0000

    Don't include stdio.h or stdarg.h here. Include syslog.h here if needed. Make err_level global again so it can be used from macros. (err_report): Use proper syslog priorities instead of assuming they are identical to err_reporting_level_t values.

--- crypto/kernel/err.c
+++ crypto/kernel/err.c
@@ -44,13 +44,16 @@
 
 #include "err.h"
 
-#include <stdio.h>
-#include <stdarg.h>
+#ifdef ERR_REPORTING_SYSLOG
+# ifdef HAVE_SYSLOG_H
+#  include <syslog.h>
+# endif
+#endif
 
 
 /*  err_level reflects the level of errors that are reported  */
 
-static err_reporting_level_t err_level = err_level_none;
+err_reporting_level_t err_level = err_level_none;
 
 /* err_file is the FILE to which errors are reported */
 
@@ -91,7 +94,39 @@ err_report(int priority, char *format, ...) {
 	  /*      fprintf(err_file, "\n"); */
     }
 #ifdef ERR_REPORTING_SYSLOG
-    vsyslog(priority, format, args);
+    if (1) { /* FIXME: Make this a runtime option. */
+      int syslogpri;
+
+      switch (priority) {
+      case err_level_emergency:
+	syslogpri = LOG_EMERG;
+	break;
+      case err_level_alert:
+	syslogpri = LOG_ALERT;
+	break;
+      case err_level_critical:
+	syslogpri = LOG_CRIT;
+	break;
+      case err_level_error:
+	syslogpri = LOG_ERR;
+	break;
+      case err_level_warning:
+	syslogpri = LOG_WARNING;
+	break;
+      case err_level_notice:
+	syslogpri = LOG_NOTICE;
+	break;
+      case err_level_info:
+	syslogpri = LOG_INFO;
+	break;
+      case err_level_debug:
+      case err_level_none:
+      default:
+	syslogpri = LOG_DEBUG;
+	break;
+      }
+
+      vsyslog(syslogpri, format, args);
 #endif
     va_end(args);
   }
