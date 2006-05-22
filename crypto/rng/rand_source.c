c78bc15e3b6089971de8c8e4d875ea0d895891ed
David McGrew <mcgrew@cisco.com>
2006-05-22 21:32:55 +0000

    rng improvements, mostly for MSVC

--- crypto/rng/rand_source.c
+++ crypto/rng/rand_source.c
@@ -8,7 +8,7 @@
  */
 /*
  *	
- * Copyright(c) 2001-2005 Cisco Systems, Inc.
+ * Copyright(c) 2001-2006 Cisco Systems, Inc.
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -43,18 +43,37 @@
  */
 
 #include "config.h"
-#include "rand_source.h"
 
 #ifdef DEV_URANDOM
 # include <fcntl.h>          /* for open()  */
 # include <unistd.h>         /* for close() */
+#elif (_MSC_VER >= 1400)
+#define _CRT_RAND_S
+# include <stdlib.h>         
+# include <stdio.h>
 #else
 # include <stdio.h>
 #endif
 
-/* global dev_rand_fdes is file descriptor for /dev/random */
+#include "rand_source.h"
 
-static int dev_random_fdes = -1;
+
+/* 
+ * global dev_rand_fdes is file descriptor for /dev/random 
+ * 
+ * This variable is also used to indicate that the random source has
+ * been initialized.  When this variable is set to the value of the
+ * #define RAND_SOURCE_NOT_READY, it indicates that the random source
+ * is not ready to be used.  The value of the #define
+ * RAND_SOURCE_READY is for use whenever that variable is used as an
+ * indicator of the state of the random source, but not as a file
+ * descriptor.
+ */
+
+#define RAND_SOURCE_NOT_READY (-1)
+#define RAND_SOURCE_READY     (17)
+
+static int dev_random_fdes = RAND_SOURCE_NOT_READY;
 
 
 err_status_t
@@ -68,10 +87,12 @@ rand_source_init(void) {
   dev_random_fdes = open(DEV_URANDOM, O_RDONLY);
   if (dev_random_fdes < 0)
     return err_status_init_fail;
+#elif (_MSC_VER >= 1400)
+  dev_random_fdes = RAND_SOURCE_READY;
 #else
   /* no random source available; let the user know */
   fprintf(stderr, "WARNING: no real random source present!\n");
-  dev_random_fdes = 17;
+  dev_random_fdes = RAND_SOURCE_READY;
 #endif
   return err_status_ok;
 }
@@ -87,6 +108,20 @@ rand_source_get_octet_string(void *dest, uint32_t len) {
 #ifdef DEV_URANDOM
   if (read(dev_random_fdes, dest, len) != len)
     return err_status_fail;
+#elif (_MSC_VER >= 1400)
+  unsigned int *dst = dest;
+  while (len)
+  {
+      unsigned int val = 0;
+	  errno_t err = rand_s(&val);
+      if (err != 0)
+	      {
+              return err_status_fail;
+          }
+  
+      *dst++ = val;
+	  len--;
+  }
 #else
   /* Generic C-library (rand()) version */
   /* This is a random source of last resort */
@@ -103,7 +138,7 @@ rand_source_get_octet_string(void *dest, uint32_t len) {
 #endif
   return err_status_ok;
 }
-
+ 
 err_status_t
 rand_source_deinit(void) {
   if (dev_random_fdes < 0)
@@ -112,7 +147,7 @@ rand_source_deinit(void) {
 #ifdef DEV_URANDOM
   close(dev_random_fdes);  
 #endif
-  dev_random_fdes = -1;
+  dev_random_fdes = RAND_SOURCE_NOT_READY;
   
   return err_status_ok;  
 }
