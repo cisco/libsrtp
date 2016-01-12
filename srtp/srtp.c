da852b0c253f6494bd70ae254808669acea9ea63
jfigus <foleyj@cisco.com>
2016-01-12 09:14:46 -0500

    Resolve issue 37 by always using BE byte ordering of the RTCP trailer.  Note, this commit will break backwards compatibility of RTCP protection when an AEAD profile is used.

--- srtp/srtp.c
+++ srtp/srtp.c
@@ -2984,9 +2984,9 @@ srtp_protect_rtcp_aead (srtp_t ctx, srtp_stream_ctx_t *stream,
 	}
     }
     /* 
-     * put the idx# into network byte order and process it as AAD
+     * Process the sequence# as AAD
      */
-    tseq = htonl(*trailer);
+    tseq = *trailer;
     status = srtp_cipher_set_aad(stream->rtcp_cipher, (uint8_t*)&tseq, sizeof(srtcp_trailer_t));
     if (status) {
         return ( srtp_err_status_cipher_fail);
@@ -3131,9 +3131,9 @@ srtp_unprotect_rtcp_aead (srtp_t ctx, srtp_stream_ctx_t *stream,
     }
 
     /* 
-     * put the idx# into network byte order, and process it as AAD 
+     * Process the sequence# as AAD 
      */
-    tseq = htonl(*trailer);
+    tseq = *trailer;
     status = srtp_cipher_set_aad(stream->rtcp_cipher, (uint8_t*)&tseq, sizeof(srtcp_trailer_t));
     if (status) {
 	return ( srtp_err_status_cipher_fail);
