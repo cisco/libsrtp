796e008c0a52c855ce4cfb165b9dba8805feea66
Pascal Bühler <pabuhler@cisco.com>
2017-02-22 23:21:54 +0100

    Revert code style as requested in #188 & #185

--- srtp/srtp.c
+++ srtp/srtp.c
@@ -4195,15 +4195,23 @@ srtp_profile_get_master_key_length(srtp_profile_t profile) {
 
   switch(profile) {
   case srtp_profile_aes128_cm_sha1_80:
+    return 16;
+    break;
   case srtp_profile_aes128_cm_sha1_32:
-  case srtp_profile_aead_aes_128_gcm:
     return 16;
     break;
   case srtp_profile_null_sha1_80:
     return 16;
     break;
   case srtp_profile_aes256_cm_sha1_80:
+    return 32;
+    break;
   case srtp_profile_aes256_cm_sha1_32:
+    return 32;
+    break;
+  case srtp_profile_aead_aes_128_gcm:
+    return 16;
+    break;
   case srtp_profile_aead_aes_256_gcm:
     return 32;
     break;
@@ -4219,13 +4227,23 @@ srtp_profile_get_master_salt_length(srtp_profile_t profile) {
 
   switch(profile) {
   case srtp_profile_aes128_cm_sha1_80:
+    return 14;
+    break;
   case srtp_profile_aes128_cm_sha1_32:
+    return 14;
+    break;
   case srtp_profile_null_sha1_80:
+    return 14;
+    break;
   case srtp_profile_aes256_cm_sha1_80:
+    return 14;
+    break;
   case srtp_profile_aes256_cm_sha1_32:
     return 14;
     break;
   case srtp_profile_aead_aes_128_gcm:
+    return 12;
+    break;
   case srtp_profile_aead_aes_256_gcm:
     return 12;
     break;
