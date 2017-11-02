c9f210d98584663505e1b90f473fdb5366fe3fda
Martin Meszaros <meszaros@hft-leipzig.de>
2017-11-02 17:53:41 +0100

    apply clang format

--- test/rtp_decoder.c
+++ test/rtp_decoder.c
@@ -70,15 +70,15 @@
 #include "util.h"
 
 #ifndef timersub
-# define timersub(a, b, result)						\
-	do {								\
-		(result)->tv_sec = (a)->tv_sec - (b)->tv_sec;		\
-		(result)->tv_usec = (a)->tv_usec - (b)->tv_usec;        \
-		if ((result)->tv_usec < 0) {                            \
-			--(result)->tv_sec;				\
-			(result)->tv_usec += 1000000;			\
-		}                                                       \
-	} while (0)
+#define timersub(a, b, result)                                                 \
+    do {                                                                       \
+        (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                          \
+        (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                       \
+        if ((result)->tv_usec < 0) {                                           \
+            --(result)->tv_sec;                                                \
+            (result)->tv_usec += 1000000;                                      \
+        }                                                                      \
+    } while (0)
 #endif
 
 #define MAX_KEY_LEN 96
