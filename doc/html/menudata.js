a40c2c5dce6e104203e84bb449c4005992f4bbdf
Pascal Buhler <pabuhler@cisco.com>
2018-05-16 10:11:30 +0200

    Update generated documentation

---
+++ doc/html/menudata.js
@@ -0,0 +1,10 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Modules",url:"modules.html"},
+{text:"Data Structures",url:"annotated.html",children:[
+{text:"Data Structures",url:"annotated.html"},
+{text:"Data Fields",url:"functions.html",children:[
+{text:"All",url:"functions.html"},
+{text:"Variables",url:"functions_vars.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"}]}]}
