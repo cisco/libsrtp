cc53930d148ff29c6b98d81f281c4807c1d1521e
Richard Barnes <rlb@ipv.sx>
2021-04-05 17:31:19 -0400

    Restore functions noted by @JonathanLennox

--- crypto/include/auth.h
+++ crypto/include/auth.h
@@ -156,6 +156,16 @@ srtp_err_status_t srtp_auth_type_self_test(const srtp_auth_type_t *at);
 srtp_err_status_t srtp_auth_type_test(const srtp_auth_type_t *at,
                                       const srtp_auth_test_case_t *test_data);
 
+/*
+ * srtp_replace_auth_type(ct, id)
+ *
+ * replaces srtp's kernel's auth type implementation for the auth_type id
+ * with a new one passed in externally.  The new auth type must pass all the
+ * existing auth_type's self tests as well as its own.
+ */
+srtp_err_status_t srtp_replace_auth_type(const srtp_auth_type_t *ct,
+                                         srtp_auth_type_id_t id);
+
 #ifdef __cplusplus
 }
 #endif
