db4dc8973394b5b370f549512d578e720b8870b9
Andrey Semashev <Lastique@users.noreply.github.com>
2021-11-17 16:16:38 +0300

    Avoid initializing local char table when converting nibble to hex.

--- crypto/math/datatypes.c
+++ crypto/math/datatypes.c
@@ -64,8 +64,8 @@ static char bit_string[MAX_PRINT_STRING_LEN + 1];
 
 uint8_t srtp_nibble_to_hex_char(uint8_t nibble)
 {
-    char buf[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
-                     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    static const char buf[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
+                                  '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
     return buf[nibble & 0xF];
 }
 
